26574861295b7dee72249f6b8737b735db027a12
awojty <awojtyna@mit.edu>
2021-06-18 08:06:41 -0400

    remove menu

--- src/App.js
+++ src/App.js
@@ -205,14 +205,14 @@ function App() {
     return(
       <div className="App">
          
-        <div className="background" onClick={toggleMenu} >
+        {/* <div className="background" onClick={toggleMenu} >
            
            <svg viewBox="0 0 100 80" width="40" height="40">
      <rect width="100" height="20"></rect>
      <rect y="30" width="100" height="20"></rect>
      <rect y="60" width="100" height="20"></rect>
    </svg>
-   </div> 
+   </div>  */}
           {/* <div id="flyoutMenu"
             ref={headerRef}
             className={menuvisibility}>
